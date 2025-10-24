c408c0f312ba864d9b4ba4704fd2749d5429038d
Gaurav_Singh <mannusingh2217@gmail.com>
2025-10-24 15:26:22 +0530

    Instagram Service Related Changes

---
+++ src/main/java/com/newDew/Instagram/service/impl/LikesServiceImpl.java
@@ -0,0 +1,8 @@
+package com.newDew.Instagram.service.impl;
+
+import com.newDew.Instagram.service.LikesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikesServiceImpl implements LikesService {
+}
